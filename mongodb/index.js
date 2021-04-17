474f9dcd824edfb66e2e02b8e34d6d0aa038bdc9
Chiaka Williams <Chiaka394@gmail.com>
2021-04-17 22:37:42 +0100

    added more code

--- mongodb/index.js
+++ mongodb/index.js
@@ -1,6 +1,36 @@
-const express = require('express')
-const path = require('path')
-const handlebars = require('handlebars')
-const exphbs = require('express-handlebars')
-const {allowInsecurePrototypeAccess} = require('@handlebars/allow-prototype-acces')
-const bodyparser = require('body-parser')
+const express = require('express');
+const path = require('path');
+const handlebars = require('handlebars');
+const exphbs = require('express-handlebars');
+const {allowInsecurePrototypeAccess} = require('@handlebars/allow-prototype-access');
+const bodyparser = require('body-parser');
+
+var app = express();
+
+app.use(express.urlencoded({extended: true}));
+app.use(express.json());
+
+app.get('/', (req, res) => {
+    res.send(`
+    <h2>Welcome to Students Database</h2>
+    <h3>Click here to get access to the <b> <a href="/student/list">Database<a></b></h3>
+    `);
+});
+
+app.set('views', path.join(__dirname, '/views/'))
+
+app.engine(
+    'hbs',
+    exphbs({
+        handlebars: allowInsecurePrototypeAccess(handlebars),
+        extname: 'hbs',
+        defaultLayout: 'MainLayout',
+        layoutsDir: __dirname + '/views/layouts/',
+    })
+);
+
+app.set('view engine', 'hbs');
+
+app.listen(3000, () => {
+    console.log('server started at port 3000')
+});
